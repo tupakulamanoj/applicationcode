c92531ce58612f7c776d5087d1378179311f332f
pavanjvm <pavanprasad05062gmail.com>
2025-04-22 11:34:57 +0530

    .

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my ArgoCD demo app! 🚀');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
